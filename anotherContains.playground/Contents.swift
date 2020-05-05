39f6fd8ad6cb96bd1a3d125049764c6c05bea269
John Holowesko <jholowesko@gmail.com>
2020-05-04 23:57:38 -0400

    Challenge Complete

--- anotherContains.playground/Contents.swift
+++ anotherContains.playground/Contents.swift
@@ -1,12 +1,19 @@
 import Foundation
 
 extension String {
-    
+    func anotherContains(string: String) {
+        let text = self.lowercased()
+        let subtext = string.lowercased()
+        
+        if text.range(of: subtext) != nil {
+            print("True")
+        }
+        else {
+            print("False")
+        }
+    }
 }
 
-
-// test cases
-print("Where is WaLdO".anotherContains("WALDO")) // true
-print("Where is WaLdO".anotherContains("where")) // true
-print("Where is WaLdO".anotherContains("is wA")) // true
-print("Where is WaLdO".anotherContains("nOPe"))  // false
+/// String Extention Tests
+"Hello my name is Bob".anotherContains(string: "noob")
+"Where is WaLdO".anotherContains(string: "WALDO")
